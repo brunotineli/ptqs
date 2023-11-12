f785799fc7d7c60bce764038ef720346f75aae01
Bruno Tineli <bruno.tineli@gmail.com>
2023-11-11 22:04:35 -0300

    Little fixes and improvements

--- lojinha/src/database/2_DDL_INSERT.sql
+++ lojinha/src/database/2_DDL_INSERT.sql
@@ -1,23 +1,23 @@
 INSERT INTO produto (
 nome, marca, tamanho, itens_inclusos, valor
 ) values (
-"PS5", "Marca", "GRANDE", "2 controles e 3 jogos", 1.55
+"PS5", "Sony", "GRANDE", "2 controles e 3 jogos", 1.99
 );
 
 INSERT INTO produto (
 nome, marca, tamanho, itens_inclusos, valor
 ) values (
-"PS6", "Sony", "GRANDE", "3 controles e 3 jogos", 2.55
+"PS6", "Sony", "GRANDE", "3 controles e 3 jogos", 2.99
 );
 
 INSERT INTO produto (
 nome, marca, tamanho, itens_inclusos, valor
 ) values (
-"PS7", "Sony", "MEDIO", "3 controles e 4 jogos", 3.55
+"PS7", "Sony", "MEDIO", "3 controles e 4 jogos", 3.99
 );
 
 INSERT INTO produto (
 nome, marca, tamanho, itens_inclusos, valor
 ) values (
-"PS8", "Sony", "PEQUENO", "4 controles e 4 jogos", 4.55
+"PS8", "Sony", "PEQUENO", "4 controles e 4 jogos", 4.99
 );
